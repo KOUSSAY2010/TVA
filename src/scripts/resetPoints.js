f987a602efbf979f6431c8c5b3ff37d1942a0b23
KOUSSAY2010 <K4781884@gmail.com>
2026-09-23 14:48:14 +0100

    fix: correct User model import case

--- src/scripts/resetPoints.js
+++ src/scripts/resetPoints.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import config from '../config/index.js';
-import User from '../models/user.js';
+import User from '../models/User.js';
 
 async function resetAllPoints() {
   try {
